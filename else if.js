53e4f06e17f52f6d996801be7ffa03c9af703d3f
Mythri reddy <mythrireddy1803@gmail.com>
2026-03-12 12:23:32 +0530

    Add account opening logic based on adhar and pan

---
+++ else if.js
@@ -0,0 +1,9 @@
+adhar=false;
+pan=true;
+if(adhar){
+    console.log('you can oen account in our bank - using adhar');
+}else if(pan){
+    console.log('you can open account in our bank - using pan');
+}else{
+    console.log('you can not open account in our bank');
+}
